6a90497295bb0f302562188fff966ef4d99fac3a
Nassir Isaf <njisaf@gmail.com>
2016-10-09 18:30:56 -0700

    honestly no idea what i'm writing

--- lab-nassir/model/pic.js
+++ lab-nassir/model/pic.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const debug = require('debug')('cuttlefish:pic');
+const createError = require('http-errors');
+
 const mongoose = require('mongoose');
 
 const picSchema = mongoose.Schema({
@@ -9,5 +12,17 @@ const picSchema = mongoose.Schema({
   imageURI: {type: String, required: true, unique: true},
   created: {type: Date, default: Date.now},
 });
+
+picSchema.methods.findByIdAndUpdate = function(id, _pic) {
+  debug('Hit findByIdAndUpdate');
+  Pic.findById(id)
+  .catch(err => Promise.reject(createError(404, err.message)))
+  .then(pic => {
+    for (var key in pic) {
+      if (key === 'imageURI') continue;
+      if (_pic[key]) pic[key] = _pic[key];
+    }
+  });
+};
 
 module.exports = mongoose.model('pic', picSchema);
